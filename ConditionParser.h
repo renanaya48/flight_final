934873f52a5b7e1988270609ed3f26d90595e1b5
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 19:46:18 +0200

    Update ConditionParser.h

--- ConditionParser.h
+++ ConditionParser.h
@@ -9,20 +9,39 @@
 #define PROJECTNUM1_CONDITIONPARSER_H
 
 
+/**
+ * ConditionParser inheritors Command
+ */
 class ConditionParser : public Command {
+    //members
     list<CommandExpression *> expressionCommandList;
     CommandMap* commandMap;
     BoolExpression* boolExpression;
     SymbolTable* symbolTable;
 
 public:
+    /**
+    * constructor
+    */
     ConditionParser(){}
-
+    /**
+    * constructor
+    */
     ConditionParser(CommandMap* commandMap,SymbolTable* symbolMap);
-
+      /**
+     * parser the condition
+     * @param script iterator
+     * @return 0 when finish
+     */
     virtual int execute(vector<string>::iterator &vectorIt);
+     /**
+     * create new command
+     * @param vectorIt the iterator
+     */
     void createCommand(vector<string>::iterator &vectorIt);
-
+    /**
+    *distructor
+    */
     ~ConditionParser(){}
 };
 
