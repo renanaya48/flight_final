d0fd6ef17c2e3da4eda8ef6bc260de7e18e35ff3
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 19:08:08 +0200

    Update CheckConnection.h

--- CheckConnection.h
+++ CheckConnection.h
@@ -1,21 +1,37 @@
 #ifndef PROJECT1_CHECKCONNECTION_H
 #define PROJECT1_CHECKCONNECTION_H
 
-
+/**
+ *  CheckConnection class.
+ */
 class CheckConnection {
+    //member
     bool isConnected;
 
 public:
+     /**
+     * construstor
+     */
     CheckConnection(){
         this->isConnected = false;
     }
+    /**
+     * set the connection according to the flag
+     * @param flag the new value
+     */
     void setConnect(bool flag){
         this->isConnected  = flag;
     }
+     /**
+     * get the connection status
+     * @return the connection status
+     */
     bool getConnect() const{
         return this->isConnected;
     }
-
+    /**
+    * distructor
+    */
     ~CheckConnection(){}
 };
 
