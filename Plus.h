8b57271e68fffa2ce25a88349b30d0a7dcda9049
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 23:20:41 +0200

    Update Plus.h

--- Plus.h
+++ Plus.h
@@ -6,23 +6,48 @@
 #ifndef PROJECT1_PLUS_H
 #define PROJECT1_PLUS_H
 
-
+/**
+ * Plus class inheritors Expression, BinaryExpression
+ */
 class Plus : public Expression,public BineryExpression {
 public:
+     /**
+ * constructor-2 Expressions
+ * @param leftArgument Expression
+ * @param rightArgument Expression
+ */
     Plus(Expression *leftArgument, Expression *rightArgument) : BineryExpression(leftArgument, rightArgument
                                                                                  ) {
     }
-
+ /**
+ * constructor-2 double
+ * @param leftArgument double
+ * @param rightArgument double
+ */
     Plus(double leftArgument, double rightArgument) : BineryExpression(new Num(leftArgument), new Num(rightArgument)
                                                                        ) {
     }
-
+    /**
+     * constructor-double and Expression
+     * @param leftArgument double
+     * @param rightArgument Expression
+     */
     Plus(double leftArgument, Expression *rightArgument) : BineryExpression(new Num(leftArgument), rightArgument
                                                                             ) {
     }
+        /**
+ * constructor- Expression and double
+ * @param leftArgument Expression
+ * @param rightArgument double
+ */
     Plus(Expression *leftArgument, double rightArgument) : BineryExpression(leftArgument, new Num(rightArgument)
                                                                             ) {
     }
+        /**
+     * calculate the value of the plus
+     * @param assignment
+     * @return the value of calculate
+     */
     virtual double calculate(map<string, double> &assignment) ;
 
     /**
@@ -30,7 +55,6 @@ public:
      * but this methods use in empty assignment.
      *
      * @return the result of the expression using
-     * @throws Exception if the expression contains a variable which is not in the assignment, an exception is thrown.
      */
     virtual double calculate() ;
 };
