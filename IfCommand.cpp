29f70e53dac6b0a15f9c33d4abc10b22acd3c219
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 22:16:28 +0200

    Update IfCommand.cpp

--- IfCommand.cpp
+++ IfCommand.cpp
@@ -1,11 +1,20 @@
 #include "IfCommand.h"
 
+/**
+ * if the condition true- do the next.
+ * @param script the iterator
+ * @return 0 when finish
+ */
 int IfCommand::execute(vector<string>::iterator &vectorIt) {
+    //if the condition is true
     if (this->boolExpression->calculateBool(this->symbolTable->getSymbols())) {
+        //iterator on the list of the commands
         list<CommandExpression *>::iterator tempIt;
 
+        //for all the list
         for (tempIt = this->commandExpressions.begin();
              tempIt != this->commandExpressions.end(); tempIt++) {
+            //execute commands
             map<string, double> symbolMap = this->symbolTable->getSymbols();
             (*tempIt)->calculate(symbolMap);
         }
