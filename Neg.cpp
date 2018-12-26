1b57be21761dde8f5463172be7ff8c721c405c95
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 22:41:36 +0200

    Update Neg.cpp

--- Neg.cpp
+++ Neg.cpp
@@ -1,14 +1,25 @@
 
 #include "Neg.h"
-
+/**
+ * constructor
+ * @param negativeNumber Expression
+ */
 Neg::Neg(Expression *negNum) {
     this->negativeNumber = negNum;
 }
-
+/**
+ * calculate the neg of the number
+ * @param assignment map
+ * @return the neg number
+ */
 double Neg::calculate(map<string, double> &assignment) {
+    //mult by (-1)
     return (-1)*this->negativeNumber->calculate(assignment);
 }
-
+/**
+ *
+ * @return the neg number
+ */
 double Neg::calculate() {
     map<string, double> temp;
     calculate(temp);
