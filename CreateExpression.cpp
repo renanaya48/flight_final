46b755be0a93db727216c82a23f074b549fa46f6
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 20:41:11 +0200

    Update CreateExpression.cpp

--- CreateExpression.cpp
+++ CreateExpression.cpp
@@ -1,6 +1,10 @@
 #include "CreateExpression.h"
 
-// shunting yard
+/**
+ * shunting yard
+ * @param str the string to make expression
+ * @return
+ */
 Expression *CreateExpression::convertToExpression(string string1) {
 
     string operatorString = "";
@@ -9,19 +13,24 @@ Expression *CreateExpression::convertToExpression(string string1) {
 
     int stringLength = 0;
     int resultLength = 0;
-
+    //go on all the string    
     while (stringLength < string1.length()) {
+        //if there is a (, put in the stack
         if (string1[stringLength] == '(') {
             operators.push("(");
+            // if there is a )
         } else if (string1[stringLength] == ')') {
             string temp;
             operatorString += ",";
+            //until the ( is in the top og the stack
             while (operators.top() != "(") {
                 temp = operators.top();
                 operatorString += temp;
                 operators.pop();
             }
+            //out of the stack
             operators.pop();
+            //if there is a operators: +-*/
         } else if (string1[stringLength] != '+' && string1[stringLength] != '-' &&
                    string1[stringLength] != '*' && string1[stringLength] != '/') {
 
@@ -31,6 +40,7 @@ Expression *CreateExpression::convertToExpression(string string1) {
             if ((stringLength == 0) && (string1[stringLength] == '-')) {
                 operatorPush = "^";
             } else {
+                //check the current char and the previous char
                 char previousChar = string1[stringLength - 1];
                 char currentChar = string1[stringLength];
                 if ((currentChar == '-') &&
@@ -43,50 +53,77 @@ Expression *CreateExpression::convertToExpression(string string1) {
 
                 }
             }
+            //add the operator to the stack
             operators.push(operatorPush);
         }
         stringLength++;
     }
-
+    //until the end
     while (!operators.empty()) {
         operatorString += operators.top();
         operators.pop();
     }
-
+    //until there are operators in the list
     while (resultLength < operatorString.length()) {
         Expression *newExpression;
         Expression *rightExpression;
         Expression *leftExpression;
         if (operatorString[resultLength] != ' ') {
+            //if the operator is +
             if (operatorString[resultLength] == '+') {
+                //set the first expression
                 rightExpression = expressions.top();
+                //out from stack
                 expressions.pop();
+                //set the second expression
                 leftExpression = expressions.top();
+                //out from stack
                 expressions.pop();
                 resultLength++;
+                //new plus expression
                 newExpression = new Plus(leftExpression, rightExpression);
+                //if the operator is *
             } else if (operatorString[resultLength] == '*') {
+                //set the first expression
                 rightExpression = expressions.top();
+                //out from stack
                 expressions.pop();
+                //set the second expression
                 leftExpression = expressions.top();
+                //out from stack
                 expressions.pop();
                 resultLength++;
+                //new mul expression
                 newExpression = new Mul(leftExpression, rightExpression);
+                //if the operator is /
             } else if (operatorString[resultLength] == '/') {
+                //set the first expression
                 rightExpression = expressions.top();
+                //out from stack
                 expressions.pop();
+                //set the second expression
                 leftExpression = expressions.top();
+                //out from stack
                 expressions.pop();
                 resultLength++;
+                //new div expression
                 newExpression = new Div(leftExpression, rightExpression);
+                //if the operator is -
             } else if (operatorString[resultLength] == '-') {
+                //set the first expression
                 rightExpression = expressions.top();
+                //out from stack
                 expressions.pop();
+                //set the second expression
                 leftExpression = expressions.top();
+                //out from stack
                 expressions.pop();
+                //new minus expression
                 newExpression = new Minus(leftExpression, rightExpression);
                 resultLength++;
+                //if the operator is ^
             } else if (operatorString[resultLength] == '^') {
+                //new neg expression
                 newExpression = new Neg(expressions.top());
                 resultLength++;
             } else if (isdigit(operatorString[resultLength])) {
@@ -118,5 +155,6 @@ Expression *CreateExpression::convertToExpression(string string1) {
             resultLength++;
         }
     }
+    //return the expression
     return expressions.top();
 }
