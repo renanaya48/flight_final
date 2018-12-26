b55873daa13f4fa2b3cb1690735c110dce6233c7
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 23:28:47 +0200

    Update PrintCommand.cpp

--- PrintCommand.cpp
+++ PrintCommand.cpp
@@ -1,11 +1,20 @@
 #include "PrintCommand.h"
-
+/**
+ *execute
+ * @param vectorIt the iterator
+ * @return 0 when done
+ */
 int PrintCommand::execute(vector<string>::iterator &vectorIt) {
     string toPrint = "";
+    //there ate chars in the string
     if((*vectorIt)[0] != '\"'){
+        //put the value in toPrint
         toPrint = (*vectorIt);
+        //no chars
     } else {
+        //until the end
         while ((*vectorIt) != ";") {
+            //get the word
             toPrint += (*vectorIt);
             vectorIt++;
         }
@@ -13,6 +22,7 @@ int PrintCommand::execute(vector<string>::iterator &vectorIt) {
     }
     vectorIt++;
 
+    //print
     cout << toPrint << endl;
     return 0;
 }
