835de96e8de7985843bc5d6a4a5fd602612804b8
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 22:30:00 +0200

    Update LoopCommand.h

--- LoopCommand.h
+++ LoopCommand.h
@@ -9,13 +9,22 @@
 #ifndef PROJECTNUM1_LOOPCOMMAND_H
 #define PROJECTNUM1_LOOPCOMMAND_H
 
-
+/**
+*LoopCommand class
+*/
 class LoopCommand : public Command  {
+    //members
     BoolExpression* boolExpression;
     list<CommandExpression*> commandExpressions;
     SymbolTable* symbolTable;
 
 public:
+        /**
+     * constructor
+     * @param boolExpres  BoolExpression
+     * @param list list<CommandExpression *>
+     * @param symbolTable  SymbolTable
+     */
     LoopCommand(BoolExpression *boolExpres,
                              list<CommandExpression *> list,
                              SymbolTable *symbolTable) {
@@ -23,7 +32,16 @@ public:
         this->commandExpressions = list;
         this->symbolTable = symbolTable;
     }
+     /**
+     * set the map
+     * @param cMap  the map to change
+     */
     void setCommandsMap(map <string,Command*> cMap);
+       /**
+     * execute
+     * @param script  the iterator
+     * @return 0 when finish
+     */
     virtual int execute(vector<string>::iterator &vectorIt);
 };
 
