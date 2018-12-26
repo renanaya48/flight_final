55938fcb967bc126c4bb44b0548103d2513adb59
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 19:10:16 +0200

    Update Command.h

--- Command.h
+++ Command.h
@@ -9,13 +9,28 @@
 
 using namespace std;
 
+/**
+ * Command class.
+ */
 class Command {
 public:
+    //member
     map<string, double> symbolTable;
 public:
+    /**
+     * constructor
+     */
     Command(){}
+    /**
+     *
+     * @param vectorIt the iterator that point to the
+     *                  place at the vector
+     * @return o if succeed
+     */
     virtual int execute(vector<string>::iterator &vectorIt) = 0;
-
+    /**
+    * distructor
+    */
     ~Command(){}
 };
 
