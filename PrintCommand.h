fbb58335e6277435c77558c3d180a80646084a0c
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 23:26:25 +0200

    Update PrintCommand.h

--- PrintCommand.h
+++ PrintCommand.h
@@ -6,14 +6,25 @@
 #include "SymbolTable.h"
 #include <map>
 #include <iostream>
-
+/**
+ * PrintCommand class inheritors Command
+ */
 class PrintCommand: public Command{
+    //member
     SymbolTable* symbolTable;
 public:
+    /**
+     * constructor
+     * @param s SymbolTable
+     */
     PrintCommand(SymbolTable *s) {
         this->symbolTable = s;
     }
-
+/**
+ * 
+ * @param vectorIt the iterator
+ * @return 0 when done
+ */
     virtual int execute(vector<string>::iterator &vectorIt);
 
 };
