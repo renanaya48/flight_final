62976df90d5813ca03766a0eaf061d51b3d8a13c
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 22:43:19 +0200

    Update Neg.h

--- Neg.h
+++ Neg.h
@@ -4,14 +4,29 @@
 #define PROJECT1_NEG_H
 
 
+/**
+ * Neg class inheritors Expression
+ */
 class Neg : public Expression {
 public:
+    //member
     Expression *negativeNumber;
-
+    /**
+     * constructor
+     * @param negativeNumber Expression
+     */
     Neg(Expression *negativeNumber);
-
+/**
+ * calculate the neg of the number
+ * @param assignment map
+ * @return the neg number
+ */
     virtual double calculate(map<string, double> &assignment);
 
+    /**
+ *
+ * @return the neg number
+ */
     virtual double calculate();
 
 };
