d9bcae1f46371f8c845a20782ef31057b9bf8890
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 23:34:10 +0200

    Update SetControlCommand.h

--- SetControlCommand.h
+++ SetControlCommand.h
@@ -4,17 +4,29 @@
 #include "Command.h"
 #include "SymbolTable.h"
 #include "StringFlightControls.h"
-
+/**
+ * SetControlCommand class inheritors Command
+ */
 class SetControlCommand: public Command {
+    //members
     StringFlightControls* stringControl;
     SymbolTable* symbols;
 
 public:
+        /**
+     * constructor
+     * @param stringC StringFlightControls
+     * @param symbol SymbolTable
+     */
     SetControlCommand(StringFlightControls* stringC, SymbolTable* symbol){
         this->symbols = symbol;
         this->stringControl = stringC;
     }
-
+/**
+ * execute
+ * @param vectorIt the iterator
+ * @return o when done
+ */
     virtual int execute(vector<string>::iterator &vectorIt);
 
 };
