b0b535744b211257596f9c1c6fc8f2c7aa1fec8b
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 23:12:04 +0200

    Update PathMap.h

--- PathMap.h
+++ PathMap.h
@@ -5,16 +5,31 @@
 #include<map>
 
 using namespace std;
-
+/**
+*PathMap
+*/
 class PathMap {
+    //member
     map<string,string> pathMap;
 public:
+    /**
+     * constructor
+     */
     PathMap(){}
+        /**
+     * add to the map
+     * @param key the key to insert
+     * @param value the value to add
+     */
     void addToPathMap(string key, string value) {
         this->pathMap.insert(pair<string,string> (key,value));
     }
     //string getBindMap(string key) const;
     //bool isInMap(string key) const;
+        /**
+     * the function returns the map
+     * @return the map
+     */
     map<string,string> getBindMap(){
         return this->pathMap;
     }
