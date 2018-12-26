7312e79f32aa2a7b6149e17b932cb0687fe1e18e
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 18:51:25 +0200

    Update BineryExpression.h

--- BineryExpression.h
+++ BineryExpression.h
@@ -6,12 +6,23 @@
 #include <iostream>
 #include <cmath>
 #include "Expression.h"
+/**
+*BineryExpression class
+*/
 class BineryExpression {
+    //members
 protected:
     Expression *leftArgument;
     Expression *rightArgument;
 
 public:
+    /**
+     * The BinaryExpression constructor by two expression and operator.
+     *
+     * @param leftArgument  the left side argument
+     * @param rightArgument the right side argument
+     * @param operator      string of the class.
+     */
     BineryExpression(Expression *leftArgument, Expression *rightArgument) {
         this->leftArgument = leftArgument;
         this->rightArgument = rightArgument;
