a41627a3327d0f2aa8fe3f68dc1c72d68c8c8680
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-26 17:21:57 +0200

    Add files via upload

---
+++ PathMap.h
@@ -0,0 +1,24 @@
+#ifndef PROJECT1_BINDMAP_H
+#define PROJECT1_BINDMAP_H
+
+#include <string>
+#include<map>
+
+using namespace std;
+
+class PathMap {
+    map<string,string> pathMap;
+public:
+    PathMap(){}
+    void addToPathMap(string key, string value) {
+        this->pathMap.insert(pair<string,string> (key,value));
+    }
+    //string getBindMap(string key) const;
+    //bool isInMap(string key) const;
+    map<string,string> getBindMap(){
+        return this->pathMap;
+    }
+
+};
+
+#endif //PROJECT1_BINDMAP_H
