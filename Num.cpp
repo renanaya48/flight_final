1e378028ef387d8b35cb83a196ae635cf0c01c75
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-26 22:46:20 +0200

    Update Num.cpp

--- Num.cpp
+++ Num.cpp
@@ -1,11 +1,22 @@
 
 #include "Num.h"
 
+/**
+ * The function return the value of the operation function.
+ *
+ * @param assignment evaluate the expression using in this assignment
+ * @return value of this num
+ * @throws Exception if is not assignment or not valid operation or number
+ */
 double Num::calculate(map<string, double> &assignment)
 {
     return this->num;
 }
-
+   /**
+ * A convenience method. Like the `evaluate(assignment)` method above, but uses an empty assignment.
+ *
+ * @return the value of this num
+ */
 double Num::calculate()
 {
     return this->num;
