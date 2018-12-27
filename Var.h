3b912be7039b50cd2c8ad69196b7a6e2b1f56645
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-27 15:44:23 +0200

    Update Var.h

--- Var.h
+++ Var.h
@@ -4,13 +4,29 @@
 #include <string>
 #include "Expression.h"
 #include "SymbolTable.h"
+/**
+ * Var class inheritors Expression
+ */
 class Var: public Expression {
+    //members
     string var;
     SymbolTable* symbols;
 public:
+     /**
+     * constructor
+     * @param v string
+     */
     Var(string v, SymbolTable* symbol);
+     /**
+     * calculate
+     * @param assignment map
+     * @return the value
+     */
     virtual double calculate(map<string, double> &assignment);
-
+    /**
+     * 
+     * @return the value
+     */
     double calculate();
 
 };
